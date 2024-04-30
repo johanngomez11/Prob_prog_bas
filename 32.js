c2d49e14ff1dc3df0e0e9ba4854ebd50feaaca66
Johann <johann272521@gmail.com>
2024-04-30 13:12:58 -0600

    32.- Crear un arreglo de 20 elementos con nombres de personas. Mostrar los elementos de la lista debiendo ir cada uno en una fila distinta.

---
+++ 32.js
@@ -0,0 +1,26 @@
+//32.- Crear un arreglo de 20 elementos con nombres de personas. Mostrar los elementos de la lista
+//debiendo ir cada uno en una fila distinta.
+const nombres = [
+    "David",
+    "Laura",
+    "Kenia",
+    "Karen",
+    "Hector",
+    "Johann",
+    "Mia",
+    "Greta",
+    "Alberto",
+    "Fernanda",
+    "Giovanny",
+    "Ricardo",
+    "Rocio",
+    "Leslie",
+    "Andrea",
+    "Monica",
+    "Ruby",
+    "Flor",
+    "Jesus",
+    "Eva"
+];
+
+nombres.forEach(nombre => console.log(nombre));
