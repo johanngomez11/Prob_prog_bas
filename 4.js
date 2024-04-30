a726a60f3dc8c760a64ce265255c2e2c9974a9f3
Johann <johann272521@gmail.com>
2024-04-30 12:29:12 -0600

    4.- Hacer un programa que imprima la suma de los 100 primeros números.

---
+++ 4.js
@@ -0,0 +1,6 @@
+//4.- Hacer un programa que imprima la suma de los 100 primeros números.
+let result = 0;
+for(let i=1; i <= 100; i++){
+    result += i;
+}
+console.log("La suma de los 100 primeros numeros es: ", result);
